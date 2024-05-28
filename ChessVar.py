d6a4995250930bfc30d2876e189cf3c0ee324c24
emily <emilyh4848@gmail.com>
2024-05-28 15:27:39 -0700

    setup ChessVar class basic structure

--- ChessVar.py
+++ ChessVar.py
@@ -10,3 +10,30 @@ class Player:
         self._name = name
         self._color = color
 
+
+class ChessVar:
+    """"""
+
+    def __init__(self):
+        self._board = []
+        # setup initial board with pieces
+        self._game_state = "UNFINISHED"
+
+    def get_game_state(self):
+        """"""
+        return self._game_state
+
+    def make_move(self, move_from, move_to):
+        """"""
+        # if move_from does not have the player's piece, return false
+        # if the move is invalid, return false
+        # if the game_state is won, return false
+
+        # else make the move
+        # remove exploded pieces
+        # update game_state if necessary
+        # return true
+
+    def print_board(self):
+        """"""
+        # print game board
