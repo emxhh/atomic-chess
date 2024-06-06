2a79cb1e7e4135fc70acc85741ca1ed3689e00cf
emily <emilyh4848@gmail.com>
2024-06-06 05:35:15 -0700

    add type hints on subclasses

--- ChessVar.py
+++ ChessVar.py
@@ -561,7 +561,7 @@ class Knight(ChessPiece):
     Inherits from ChessPiece.
     """
 
-    def __init__(self, name, color, coordinates):
+    def __init__(self, name: str, color: str, coordinates: str) -> None:
         """Initializes the instance based on name, color, and coordinates of the chess piece."""
         super().__init__(name, color, coordinates)
         self._unicode = "\u2658" if self._color == "white" else "\u265E"
@@ -583,7 +583,7 @@ class Rook(ChessPiece):
     Inherits from ChessPiece.
     """
 
-    def __init__(self, name, color, coordinates):
+    def __init__(self, name: str, color: str, coordinates: str) -> None:
         """Initializes the instance based on name, color, and coordinates of the chess piece."""
         super().__init__(name, color, coordinates)
         self._unicode = "\u2656" if self._color == "white" else "\u265C"
@@ -657,7 +657,7 @@ class Queen(ChessPiece):
     Inherits from ChessPiece.
     """
 
-    def __init__(self, name, color, coordinates):
+    def __init__(self, name: str, color: str, coordinates: str) -> None:
         """Initializes the instance based on name, color, and coordinates of the chess piece."""
         super().__init__(name, color, coordinates)
         self._unicode = "\u2655" if self._color == "white" else "\u265B"
@@ -677,7 +677,7 @@ class King(ChessPiece):
     Inherits from ChessPiece.
     """
 
-    def __init__(self, name, color, coordinates):
+    def __init__(self, name: str, color: str, coordinates: str) -> None:
         """Initializes the instance based on name, color, and coordinates of the chess piece."""
         super().__init__(name, color, coordinates)
         self._unicode = "\u2654" if self._color == "white" else "\u265A"
